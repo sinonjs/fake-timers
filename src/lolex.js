b351c3bfa08e962d883c553bd37bf5d4e8a44ed8
Rogier Schouten <github@workingcode.ninja>
2015-08-18 20:52:31 +0200

    Fix for Sinon.JS issue #766: clearTimeout() no longer clears Immediate/Interval and vice versa
    A clearTimeout(), given the ID of a timer started with setImmediate() or
    setInterval() would erroneously clear that immediate or interval

--- src/lolex.js
+++ src/lolex.js
@@ -258,6 +258,42 @@
         }
     }
 
+    function timerType(timer) {
+        if (timer.immediate) {
+            return "immediate";
+        } else if (typeof timer.interval !== "undefined") {
+            return "interval";
+        } else {
+            return "timeout";
+        }
+    }
+
+    function clearTimer(clock, timerId, ttype) {
+        if (!timerId) {
+            // null appears to be allowed in most browsers, and appears to be
+            // relied upon by some libraries, like Bootstrap carousel
+            return;
+        }
+
+        if (!clock.timers) {
+            clock.timers = [];
+        }
+
+        // in Node, timerId is an object with .ref()/.unref(), and
+        // its .id field is the actual timer id.
+        if (typeof timerId === "object") {
+            timerId = timerId.id;
+        }
+
+        if (clock.timers.hasOwnProperty(timerId)) {
+            // check that the ID matches a timer of the correct type
+            var timer = clock.timers[timerId];
+            if (timerType(timer) === ttype) {
+                delete clock.timers[timerId];
+            }
+        }
+    }
+
     function uninstall(clock, target) {
         var method,
             i,
@@ -346,25 +382,7 @@
         };
 
         clock.clearTimeout = function clearTimeout(timerId) {
-            if (!timerId) {
-                // null appears to be allowed in most browsers, and appears to be
-                // relied upon by some libraries, like Bootstrap carousel
-                return;
-            }
-
-            if (!clock.timers) {
-                clock.timers = [];
-            }
-
-            // in Node, timerId is an object with .ref()/.unref(), and
-            // its .id field is the actual timer id.
-            if (typeof timerId === "object") {
-                timerId = timerId.id;
-            }
-
-            if (clock.timers.hasOwnProperty(timerId)) {
-                delete clock.timers[timerId];
-            }
+            return clearTimer(clock, timerId, "timeout");
         };
 
         clock.setInterval = function setInterval(func, timeout) {
@@ -377,7 +395,7 @@
         };
 
         clock.clearInterval = function clearInterval(timerId) {
-            clock.clearTimeout(timerId);
+            return clearTimer(clock, timerId, "interval");
         };
 
         clock.setImmediate = function setImmediate(func) {
@@ -389,7 +407,7 @@
         };
 
         clock.clearImmediate = function clearImmediate(timerId) {
-            clock.clearTimeout(timerId);
+            return clearTimer(clock, timerId, "immediate");
         };
 
         clock.tick = function tick(ms) {
