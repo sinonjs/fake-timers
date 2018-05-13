36d7c0196099121bed5bf9baf60762bb699604fd
Benjamin Gruenbaum <inglor@gmail.com>
2018-05-13 11:19:54 +0300

    feat: respect loopLimit in runMicrotasks (#172)
    * feat: respect loopLimit in runMicrotasks

--- src/lolex-src.js
+++ src/lolex-src.js
@@ -201,6 +201,9 @@ function withGlobal(_global) {
         for (var i = 0; i < clock.jobs.length; i++) {
             var job = clock.jobs[i];
             job.func.apply(null, job.args);
+            if (clock.loopLimit && i > clock.loopLimit) {
+                throw new Error("Aborting after running " + clock.loopLimit + " timers, assuming an infinite loop!");
+            }
         }
         clock.jobs = [];
     }
