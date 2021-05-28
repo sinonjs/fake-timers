20fc8d81fc6b41671547fd98553282834e9c8f59
Carl-Erik Kopseng <carlerik@gmail.com>
2021-05-28 16:09:21 +0200

    Add some more examples from the README

--- test/check-types.ts
+++ test/check-types.ts
@@ -4,5 +4,40 @@
  */
 import * as FakeTimers from "../src/fake-timers-src";
 
+// install()
+FakeTimers.install();
+FakeTimers.install({
+    shouldAdvanceTime: true,
+    advanceTimeDelta: 40,
+});
 // #382
 FakeTimers.install({ now: 1 });
+
+// createClock()
+const clock1 = FakeTimers.createClock(1);
+FakeTimers.createClock(new Date());
+FakeTimers.createClock();
+FakeTimers.createClock(new Date(), 10);
+clock1.uninstall();
+
+// timers
+FakeTimers.timers.setInterval(
+    () => {
+        return 42;
+    },
+    10,
+    1,
+    2,
+    3,
+    4
+);
+
+// withContext
+const clock2 = FakeTimers.withGlobal({}).install();
+clock2.uninstall();
+
+// clock.*
+clock1.runToLast();
+async () => await clock1.runToLastAsync();
+clock1.runToFrame();
+clock1.setInterval((myArg) => myArg, 42, 100000);
