fc312b9ce96a4ea2c7e60bb0cccd2c604b75cdbd
Carl-Erik Kopseng <carlerik@gmail.com>
2023-08-17 20:16:35 +0200

    Make postversion script a bit more intelligent

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -4,14 +4,15 @@ cd $SCRIPT_DIR > /dev/null
 set -e
 
 # check that that origin points to the sinonjs/fake-timers repo
-git remote -v | grep 'origin.*sinonjs/fake-timers.*push' > /dev/null
+UPSTREAM=$(git config branch.main.remote)
+git config "remote.$UPSTREAM.url" | grep '.*sinonjs/fake-timers' > /dev/null
 
 if [[ $? != 0 ]]; then
-    echo "'origin' doesn't point to the sinonjs/fake-timers repo. Fix tag push manually!"
+    echo "'$UPSTREAM' doesn't point to the sinonjs/fake-timers repo. Fix tag push manually!"
     exit 1
 fi
 
-git push --follow-tags origin
+git push --follow-tags $UPSTREAM
 
 cd ..  # to root dir
 npm publish
